387c3da9b7e2571f960afbd20e667c598f6259f7
Millelang <millelang2005@gmail.com>
2024-04-09 11:02:44 +0200

    databas

--- Planering/Loggbok.md
+++ Planering/Loggbok.md
@@ -2,3 +2,6 @@
 
 ## V15 Måndag
 Idag har jag planerat och initierat projektet. Jag har skapat en projektplan och en tidsplan. Jag har skapat en projektstruktur och en första version av projektets dokumentation.
+
+## V16 Tisdag
+Idag har jag skapat en databas och börjat med att skapa en inloggningsida och registreringssida. Ett problem som jag löste idag var att jag hade problem med att få databasen att fungera, jag hade glömt att skriva in databasens namn i min .env fil.
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,66 @@ const pool = require('../db.js')
 
 
 router.get('/', (req, res) => {
-    res.render('index.njk')
+    res.render('index.njk'), {
+        username: req.session.username
+    }
+})
+
+router.get('/login', (req, res) => {
+    res.render('login.njk'), {
+        username: req.session.username
+    }
+})
+
+router.post('/login', async function (req, res) {
+
+    const username = req.body.username
+  
+    const [user] = await pool.promise().query(
+      'SELECT id, password FROM milton_users WHERE name = ?', [username]
+    )
+  
+  
+    req.session.userid = user[0].id
+    const passwordenter = req.body.password
+    
+    bcrypt.compare(passwordenter, user[0].password, function (err, result) {
+      req.session.username = username
+      console.log(req.session.username)
+      if (result) {
+        req.session.login = true
+        res.redirect('/')
+        console.log(req.session.login)
+      } else {
+        res.json({ message: 'Fel lösenord' })
+      }
+    });
+  })
+  
+
+
+router.get('/signup', (req, res) => {
+    res.render('signup.njk')
+    
+})
+
+router.post('/signup', async function (req, res) {
+    const username = req.body.username
+    const password = req.body.password
+
+    bcrypt.hash(password, 10, async function (err, hash) {
+        try {
+            const [user] = await pool.promise().query('INSERT INTO `milton_users` ( `name`, `password`) VALUES( ?, ?)', [username, hash])
+            res.redirect('/login')
+        } catch (error) {
+            console.log(error)
+            res.status(402)
+        }
     })
+
+
+
+})
+
 
 module.exports = router
